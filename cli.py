89a35029273b515f931cf02422cc182c3bce4130
adityakamath255 <aditya.kamath.270@gmail.com>
2025-08-03 23:06:57 +0530

    refactor: cli does not need to be a class

--- cli.py
+++ cli.py
@@ -1,29 +1,24 @@
 from prediction import Predictor
 
 
-class Cli:
-    def __init__(self, predictor: Predictor):
-        self.predictor = predictor
+def run_cli(predictor: Predictor):
+    """Interactively classify user-inputted messages as spam or not."""
+    print()
+    try:
+        while True:
+            message = input("Enter message to classify (or 'quit'): ")
+            if message.lower() == "quit":
+                break
 
-    def run(self) -> None:
-        """Interactively classify user-inputted messages as spam or not."""
-        print()
-        try:
-            while True:
-                message = input("Enter message to classify (or 'quit'): ")
-                if message.lower() == "quit":
-                    break
+            result = predictor.predict(message)
+            print(f"Prediction: {result['prediction']}")
+            print(f"Confidence: {result['confidence']:.2%}")
+            print()
 
-                result = self.predictor.predict(message)
-                print(f"Prediction: {result['prediction']}")
-                print(f"Confidence: {result['confidence']:.2%}")
-                print()
-
-        except KeyboardInterrupt:
-            print("\nProgram terminated by user")
+    except KeyboardInterrupt:
+        print("\nProgram terminated by user")
 
 
 if __name__ == "__main__":
     predictor = Predictor()
-    cli = Cli(predictor)
-    cli.run()
+    run_cli(predictor)
